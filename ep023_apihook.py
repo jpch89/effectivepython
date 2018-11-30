9432e0c9e1caf78703ff25f8d93eae5ffb51ffd7
jpch89 <jpch89@outlook.com>
2018-11-30 16:46:20 +0800

    update ep023

--- ep023_apihook.py
+++ ep023_apihook.py
@@ -6,3 +6,47 @@ print(names)
 ['Plato', 'Socrates', 'Aristotle', 'Archimedes']
 """
 
+from collections import defaultdict
+
+# 自定义 defaultdict 的挂钩函数
+def log_missing():
+    print('Key added')
+    return 0
+
+# 使用挂钩函数
+current = {'green': 12, 'blue': 3}
+increments = [
+    ('red', 5),
+    ('blue', 17),
+    ('orange', 9)
+]
+# defaultdict([工厂函数[, 可迭代对象]])
+result = defaultdict(log_missing, current)
+print('Before:', dict(result))
+for key, amount in increments:
+    result[key] += amount
+print('After:', dict(result))
+"""
+Before: {'green': 12, 'blue': 3}
+Key added
+Key added
+After: {'green': 12, 'blue': 20, 'red': 5, 'orange': 9}
+"""
+
+# 使用带状态的闭包
+def increment_with_report(current, increments):
+    added_count = 0
+
+    def missing():
+        nonlocal added_count  # Stateful closure
+        added_count += 1
+        return 0
+
+    result = defaultdict(missing, current)
+    for key, amount in increments:
+        result[key] += amount
+
+    return result, added_count
+
+result, count = increment_with_report(current, increments)
+assert count == 2
