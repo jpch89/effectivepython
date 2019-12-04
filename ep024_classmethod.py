8ac3826169e2d1b287c36e153b8dccb7ee40a609
团子大圆帅 <jpch89@outlook.com>
2019-12-04 20:29:13 +0800

    add 24

--- ep024_classmethod.py
+++ ep024_classmethod.py
@@ -92,3 +92,60 @@ print('There are', result, 'lines')
 """
 There are 7 lines
 """
+
+# 下面的代码仅用作说明，并没有通过测试，所以注释掉了
+"""
+class GenericInputData(object):
+    def read(self):
+        raise NotImplementedError
+
+    @classmethod
+    def generate_inputs(cls, config):
+        raise NotImplementedError
+
+
+class PathInputData(GenericInputData):
+    # 团子注：这里省略了 __init__
+
+    def read(self):
+        return open(self.path).read()
+    
+    @classmethod
+    def generate_inputs(cls, config):
+        data_dir = config['data_dir']
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+
+class GenericWorker(object):
+    # ...
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
+
+    @classmethod
+    def create_workers(cls, input_class, config):
+        workers = []
+        for input_data in input_class.generate_inputs(config):
+            workers.append(cls(input_data))
+        return workers
+    
+
+class LineCountWorker(GenericWorker):
+    # 团子注：这里需要定义 map 和 reduce 方法
+    pass
+
+
+def mapreduce(worker_class, input_class, config):
+    workers = worker_class.create_workers(input_class, config)
+    return execute(workers)
+
+
+with TemporaryDirectory() as tmpdir:
+    write_test_files(tmpdir)
+    config = {'data_dir': tmpdir}
+    result = mapreduce(LineCountWorker, PathInputData, config)
+"""
